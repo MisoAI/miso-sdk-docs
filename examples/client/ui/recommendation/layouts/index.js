10e833b63995054df03a660fa505886f9b8c42e2
simonpai <simon.pai@gmail.com>
2023-03-15 15:38:43 +0800

    Fine tune

--- examples/client/ui/recommendation/layouts/index.js
+++ examples/client/ui/recommendation/layouts/index.js
@@ -3,7 +3,7 @@ const apiKey = btoa(atob('AKeWVvgoLi4KMmO7wM6PpDNcn7tgThWww0OlWxQL').split('').r
 MisoClient.plugins.use('std:ui');
 
 const client = new MisoClient(apiKey);
-client.context.user_id = '1cvu70wkn3c6u45s';
+//client.context.user_id = '1cvu70wkn3c6u45s';
 
 const unit = client.units.get();
 unit.useApi('user_to_products', { rows: 6 });
