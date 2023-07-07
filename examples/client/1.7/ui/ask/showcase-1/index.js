992dd5e99bc9c81fd4fad549e4feb4e3885091e1
simonpai <simon.pai@gmail.com>
2023-07-07 11:24:04 +0800

    Upgrade SDK version in examples and in-site demo

--- examples/client/1.7/ui/ask/showcase-1/index.js
+++ examples/client/1.7/ui/ask/showcase-1/index.js
@@ -16,39 +16,35 @@ keyInput.addEventListener('keydown', (e) => {
 const followUpsSection = document.getElementById('follow-ups');
 const relatedResourcesContainer = document.getElementById('related-resources');
 
-const TEMPLATE_STRING = `
+function render({ parentQuestionId }) {
+  return `
 <div class="container">
-<miso-ask class="query-container" visible-when="initial loading" parent-question-id="{{parentQuestionId}}">
-  <miso-query>
-    <input class="input" placeholder="Ask a follow-up question">
-  </miso-query>
-</miso-ask>
-<miso-ask visible-when="ready" logo="false" parent-question-id="{{parentQuestionId}}">
-  <hr>
-  <div class="phrase question">And then you asked about...</div>
-  <miso-question></miso-question>
-  <miso-answer></miso-answer>
-  <miso-feedback></miso-feedback>
-  <div class="phrase sources">My reply is based on the following:</div>
-  <miso-sources></miso-sources>
-</miso-ask>
+  <miso-ask class="query-container" visible-when="initial loading" parent-question-id="${parentQuestionId}">
+    <miso-query>
+      <input class="input" data-role="input" placeholder="Ask a follow-up question">
+      <div class="autocomplete" data-role="autocomplete">
+        <ol class="suggestion-list" data-role="suggestion-list"></ol>
+      </div>
+    </miso-query>
+  </miso-ask>
+  <miso-ask visible-when="ready" logo="false" parent-question-id="${parentQuestionId}">
+    <hr>
+    <div class="phrase question">And then you asked about...</div>
+    <miso-question></miso-question>
+    <miso-answer></miso-answer>
+    <miso-feedback></miso-feedback>
+    <div class="phrase sources">My reply is based on the following:</div>
+    <miso-sources></miso-sources>
+  </miso-ask>
 </div>
-`.trim();
-
-const template = (data) => {
-  let html = TEMPLATE_STRING;
-  for (const key of Object.keys(data)) {
-    const value = data[key];
-    html = html.replaceAll(`{{${key}}}`, value);
-  }
-  return html;
-};
+`;
+}
 
 function setup(workflow) {
   // follow-up questions:
   // when a answer is fully populated, insert a new section for the follow-up question
   workflow.on('done', () => {
-    followUpsSection.insertAdjacentHTML('beforeend', template({ parentQuestionId: workflow.questionId }));
+    followUpsSection.insertAdjacentHTML('beforeend', render({ parentQuestionId: workflow.questionId }));
   });
   // follow-up questions:
   // when a new query starts, associate the last section container (for related resources) to that workflow
